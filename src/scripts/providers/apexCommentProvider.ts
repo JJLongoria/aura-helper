0df39d936fec8cbdbd56e394528c0ff9917df91f
Juan José Longoria <juan.longoria.lopez@accenture.com>
2021-11-26 21:37:12 +0100

    Converted providers/apexCommentProvider.js file into ts file

--- src/scripts/providers/apexCommentProvider.ts
+++ src/scripts/providers/apexCommentProvider.ts
@@ -1,35 +1,37 @@
-const vscode = require('vscode');
-const config = require('../core/config');
+import * as vscode from 'vscode';
+import { Config } from '../core/config';
+import { ProviderUtils } from './utils';
+import { MarkDownStringBuilder } from '../output';
 const { FileChecker } = require('@aurahelper/core').FileSystem;
 const CompletionItemKind = vscode.CompletionItemKind;
-const ProviderUtils = require('./utils');
-const MarkDownStringBuilder = require('../output/markdownStringBuilder');
 
-exports.provider = {
-    provideCompletionItems(document, position) {
+export class ApexCompletionProvider implements vscode.CompletionItemProvider<vscode.CompletionItem> {
+    provideCompletionItems(document: vscode.TextDocument, position: vscode.Position): vscode.ProviderResult<vscode.CompletionItem[] | vscode.CompletionList<vscode.CompletionItem>> {
         let items;
         if (FileChecker.isApexClass(document.uri.fsPath) || FileChecker.isApexTrigger(document.uri.fsPath)) {
             items = provideApexCompletion(document, position);
         }
         return Promise.resolve(items);
     }
-}
+};
 
-function provideApexCompletion(document, position) {
-    let items;
+function provideApexCompletion(document: vscode.TextDocument, position: vscode.Position): vscode.CompletionItem[] | undefined {
+    let items: vscode.CompletionItem[] | undefined;
     const line = document.lineAt(position.line).text;
     if (line.indexOf('/**') !== -1) {
-        if (!config.getConfig().autoCompletion.activeApexCommentSuggestion)
-            return Promise.resolve(undefined);
+        if (!config.getConfig().autoCompletion.activeApexCommentSuggestion) {
+            return undefined;
+        }
         items = getCommentCompletionItem(position);
     }
     return items;
 }
 
-function getCommentCompletionItem(position) {
+function getCommentCompletionItem(position: vscode.Position) {
     const items = [];
-    if (!config.getConfig().autoCompletion.activeApexCommentSuggestion)
-        return Promise.resolve(undefined);
+    if (!config.getConfig().autoCompletion.activeApexCommentSuggestion) {
+        return undefined;
+    }
     const documentation = new MarkDownStringBuilder().appendMarkdown('Add an Apex Comment with the user defined template\n\n');
     const options = ProviderUtils.getCompletionItemOptions('Apex Comment', documentation.build(), undefined, true, CompletionItemKind.Snippet);
     const command = ProviderUtils.getCommand('Apex Comment', 'aurahelper.completion.apex', [position, "comment"]);
