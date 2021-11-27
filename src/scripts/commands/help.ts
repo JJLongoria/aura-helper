addc75664e6931a715438d38d5c6e48783932026
Juan José Longoria <juanjoselongoria@gmail.com>
2021-11-27 22:43:43 +0100

    Converted commands/help.js file into ts file

--- src/scripts/commands/help.ts
+++ src/scripts/commands/help.ts
@@ -1,4 +1,4 @@
-const vscode = require('vscode');
-exports.run = function () {
+import * as vscode from 'vscode';
+export function run() {
     vscode.env.openExternal(vscode.Uri.parse('https://github.com/JJLongoria/aura-helper/wiki'));
-}
+};
