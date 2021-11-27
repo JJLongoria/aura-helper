7af583dff55de27dd954bbcca0cac4aebd4302a2
Juan José Longoria <juan.longoria.lopez@accenture.com>
2021-11-27 17:18:24 +0100

    Converted commands/compressXML.js file into ts file

--- src/scripts/commands/compressXML.ts
+++ src/scripts/commands/compressXML.ts
@@ -1,31 +1,32 @@
-const vscode = require('vscode');
-const OutputChannel = require('../output/outputChannnel');
-const Config = require('../core/config');
-const NotificationManager = require('../output/notificationManager');
-const Paths = require('../core/paths');
+import * as vscode from 'vscode';
+import { OutputChannel, NotificationManager } from '../output';
+import { Config } from '../core/config';
+import { Paths } from '../core/paths';
 const CLIManager = require('@aurahelper/cli-manager');
 const XMLCompressor = require('@aurahelper/xml-compressor');
 
-exports.run = function (fileUri) {
+export function run(fileUri: vscode.Uri): void {
     try {
         let filePath;
         if (fileUri) {
             filePath = fileUri.fsPath;
         } else {
             let editor = vscode.window.activeTextEditor;
-            if (editor)
+            if (editor){
                 filePath = editor.document.uri.fsPath;
+            }
         }
-        if (filePath)
+        if (filePath) {
             compressFile(filePath);
-        else
+        } else {
             NotificationManager.showError('Any file selected or opened on editor for compress');
+        }
     } catch (error) {
         NotificationManager.showCommandError(error);
     }
-}
+};
 
-function compressFile(filePath) {
+function compressFile(filePath: string): void {
     let editor = vscode.window.activeTextEditor;
     if (editor && editor.document.uri.fsPath === filePath) {
         compress(filePath);
@@ -34,20 +35,20 @@ function compressFile(filePath) {
     }
 }
 
-function compress(filePath) {
+function compress(filePath: string): void {
     const sortOrder = Config.getXMLSortOrder();
     if (Config.useAuraHelperCLI()) {
         const cliManager = new CLIManager(Paths.getProjectFolder(), Config.getAPIVersion(), Config.getNamespace());
         cliManager.compress(filePath, sortOrder).then(() => {
             OutputChannel.outputLine('XML file compressed successfully');
-        }).catch((error) => {
+        }).catch((error: any) => {
             throw error;
         });
     } else {
         const compressor = new XMLCompressor(filePath, sortOrder);
         compressor.compress().then(() => {
             OutputChannel.outputLine('XML file compressed successfully');
-        }).catch((error) => {
+        }).catch((error: Error) => {
             throw error;
         });
     }
