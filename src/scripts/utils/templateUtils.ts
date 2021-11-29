b9b7ffd964ac47a760e2103d590a6e8535362035
Juan José Longoria <juanjoselongoria@gmail.com>
2021-11-30 00:18:56 +0100

    Create new types to handle Apex Comments

--- src/scripts/utils/templateUtils.ts
+++ src/scripts/utils/templateUtils.ts
@@ -3,14 +3,53 @@ import { Paths } from "../core/paths";
 const { FileChecker, FileReader } = require('@aurahelper/core').FileSystem;
 const { Utils } = require('@aurahelper/core').CoreUtils;
 
-export class ApexCommentTemplate {
-    
+export interface ApexCommentTagKeyword {
+    name: string;
+    source?: string;
+    message?: string;
+}
+
+export interface ApexCommentTagData {
+    tagData: ApexCommentTag;
+    tag: ApexCommentTag;
+    tagName: string;
+}
+
+export interface ApexCommentTag {
+    equalsTo?: string;
+    symbol?: string;
+    keywords?: ApexCommentTagKeyword[];
+    template?: string;
+    multiple?: boolean;
+    anywhere?: boolean;
+    source?: string;
+}
+
+export interface ApexCommentsObjectData {
+    tags: string[];
+    template: string[]
+}
+
+export interface ApexCommentsData {
+    class: ApexCommentsObjectData;
+    interface?: ApexCommentsObjectData;
+    enum?: ApexCommentsObjectData;
+    method: ApexCommentsObjectData;
+    constructor?: ApexCommentsObjectData;
+    variable: ApexCommentsObjectData;
+    property?: ApexCommentsObjectData;
+}
+
+export interface ApexCommentTemplate {
+    tagSymbol: string;
+    tags: any;
+    comments: ApexCommentsData;
 }
 
 export class TemplateUtils {
 
-    static getApexCommentTemplate(custom: boolean) {
-        let templateContent;
+    static getApexCommentTemplate(custom: boolean): ApexCommentTemplate | undefined {
+        let templateContent: ApexCommentTemplate | undefined;
         if (custom) {
             if (FileChecker.isExists(Paths.getApexCommentUserTemplate())) {
                 templateContent = JSON.parse(FileReader.readFileSync(Paths.getApexCommentUserTemplate()));
@@ -23,11 +62,11 @@ export class TemplateUtils {
         return templateContent;
     }
 
-    static getTagsDataBySource(sources: any | any[], comment: any) {
+    static getTagsDataBySource(sources: string | string[], comment: ApexCommentTemplate): any {
         const result: any = {};
         sources = Utils.forceArray(sources);
         if (!applicationContext.parserData.template) {
-            return;
+            return undefined;
         }
         if (comment && comment.tags && Utils.hasKeys(comment.tags)) {
             let tagName;
@@ -39,7 +78,7 @@ export class TemplateUtils {
                 }
                 tagData = comment.tags[tagName];
                 if (tag && tagData && sources.includes(tag.source)) {
-                    const data = {
+                    const data: ApexCommentTagData = {
                         tagData: tagData,
                         tag: tag,
                         tagName: tagName
